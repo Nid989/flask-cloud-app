dfbd67ffa5fbe492eb79db871e8ebb77edd1da87
Nid989 <75028682+Nid989@users.noreply.github.com>
2021-11-09 18:11:46 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -10,14 +10,14 @@ def index():
 @app.route("/service", methods=["POST"])
 def service():
 
-    service_req = req.get("http://127.0.0.1:8080/get-service-area")
+    service_req = req.get("http://13.233.19.226:8080/get-service-area")
     city_list = service_req.json()['cities_arr']
     city_string = '\n'.join(city_list)
-    # price_req = req.get("http://127.0.0.1:8081/get-prices")
-    # price_list = price_req.json()['price_arr']
-    # price_string = '\n'.join(price_list)
-    # return render_template("main.html", service=f"{city_string}", price = f"{price_string}")
-    return render_template("main.html", service=f"{city_string}")
+    price_req = req.get("http://13.233.19.226:8081/get-prices")
+    price_list = price_req.json()['price_arr']
+    price_string = '\n'.join(price_list)
+    return render_template("main.html", service=f"{city_string}", price = f"{price_string}")
+#     return render_template("main.html", service=f"{city_string}")
 
 
 if __name__ == '__main__':
